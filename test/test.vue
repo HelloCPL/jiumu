a97049f9e20d00260ac65e311362ae7f72c8e433
陈沛良 <cpl@cszysoft.com>
2022-06-30 21:51:44 +0800

    test: 测试提交

--- test/test.vue
+++ test/test.vue
@@ -7,6 +7,8 @@
 import {defineAsyncComponent} from "vue"
 const A = defineAsyncComponent(() => import('./A.vue'))
 
+const aa = "123";
+
 // npm i -D eslint babel-eslint eslint-config-airbnb eslint-plugin-import eslint-plugin-jsx-a11y eslint-plugin-react
 
 </script>
