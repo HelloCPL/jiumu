edda22c513fea78f2aff72743930335ec7cc6fdd
陈一枝 <1142805207@qq.com>
2022-07-19 18:05:40 +0800

    feature: 增加弹框容器

---
+++ packages/pc/src/components/Dialog/index.vue
@@ -0,0 +1,89 @@
+<!--
+  @describe: 弹窗容器
+  @author: cpl
+  @create: 2022-07-19 16:14:09
+-->
+
+<template>
+  <ElDialog
+    v-model="isShow"
+    append-to-body
+    destroy-on-close
+    custom-class="dialog-wrapper"
+    :draggable="draggable"
+    :open-delay="50"
+    :close-on-click-modal="false"
+    :close-on-press-escape="false"
+  >
+    <template #header>
+      <div class="w-full h-6 text-lg g-line-1">{{ title }}</div>
+    </template>
+    <template #footer>
+      <slot name="footer">
+        <ElButton @click="isShow = false">取消</ElButton>
+        <ElButton type="primary" @click="$emit('confirm')">确认</ElButton>
+      </slot>
+    </template>
+    <div class="w-full g-scroll-y dialog-content">
+      <slot></slot>
+    </div>
+  </ElDialog>
+</template>
+
+<script lang="ts" setup>
+import { ElDialog, ElButton } from 'element-plus'
+import { ref, onMounted, onUnmounted } from 'vue'
+const isShow = ref<boolean>(false)
+onMounted(() => {
+  isShow.value = true
+})
+onUnmounted(() => {
+  isShow.value = false
+})
+
+defineEmits(['confirm'])
+
+type Props = {
+  title?: string
+  draggable?: boolean
+  width?: string | number
+  [x: string]: any // 其余参数跟 ElDialog 参数保持一致
+}
+withDefaults(defineProps<Props>(), {
+  title: '',
+  draggable: true,
+  width: '50%'
+})
+</script>
+
+<style lang="scss">
+.dialog-wrapper {
+  .el-dialog__header {
+    width: 100%;
+    padding-top: 12px;
+    padding-bottom: 12px;
+    border-bottom: 1px solid var(--jm-color-border-light);
+  }
+
+  .el-dialog__headerbtn {
+    top: 12px;
+    right: 10px;
+    width: 24px;
+    height: 24px;
+  }
+  .el-dialog__body {
+    padding-top: 0;
+    padding-bottom: 0;
+  }
+
+  .el-dialog__footer {
+    border-top: 1px solid var(--jm-color-border-light);
+    padding-top: 12px;
+    padding-bottom: 12px;
+  }
+
+  .dialog-content {
+    max-height: 65vh;
+  }
+}
+</style>
--- packages/pc/src/views/test/index.vue
+++ packages/pc/src/views/test/index.vue
@@ -1,13 +1,72 @@
 <template>
   <div class="box">
+    nihao
+    <button @click="open">按钮</button>
+    <Dialog title="大标题" v-if="show" :draggable="true"  @close="close" @confirm="confirm">
+    你好
+
+    <p>邯郸市三等奖</p>
+    <p>邯郸市三等奖</p>
+    <p>邯郸市三等奖</p>
+    <p>邯郸市三等奖</p>
+    <p>邯郸市三等奖</p>
+    <p>邯郸市三等奖</p>
+    <p>邯郸市三等奖</p>
+    <p>邯郸市三等奖</p>
+    <p>邯郸市三等奖</p>
+    <p>邯郸市三等奖</p>
+    <p>邯郸市三等奖</p>
+    <p>邯郸市三等奖</p>
+    <p>邯郸市三等奖</p>
+    <p>邯郸市三等奖</p>
+    <p>邯郸市三等奖</p>
+    <p>邯郸市三等奖</p>
+    <p>邯郸市三等奖</p>
+    <p>邯郸市三等奖</p>
+    <p>邯郸市三等奖</p>
+    <p>邯郸市三等奖</p>
+    <p>邯郸市三等奖</p>
+    <p>邯郸市三等奖</p>
+    <p>邯郸市三等奖</p>
+    <p>邯郸市三等奖</p>
+    <p>邯郸市三等奖</p>
+    <p>邯郸市三等奖</p>
+    <p>邯郸市三等奖</p>
+    <p>邯郸市三等奖</p>
+    <p>邯郸市三等奖</p>
+    <p>邯郸市三等奖</p>
+    <p>邯郸市三等奖</p>
+    <p>邯郸市三等奖</p>
+    <p>邯郸市三等奖</p>
+    <p>邯郸市三等奖</p>
+    <p>邯郸市三等奖</p>
+    <p>邯郸市三等奖</p>
+    <p>邯郸市三等奖</p>
+    <p>邯郸市三等奖</p>
+    </Dialog>
   </div>
 </template>
 
 <script setup lang="ts">
+import Dialog from '@/components/Dialog/index.vue'
+import {ref} from 'vue'
+const show = ref(false)
 
+const close = () => {
+  console.log('关闭了');
+  show.value = false
+}
+const open = () => {
+  console.log(111, show.value);
+  show.value = !show.value
+}
 
+const confirm = () => {
+  console.log('confirm');
+  show.value = false
+
+}
 </script>
 
 <style lang="scss" scoped>
-
 </style>
