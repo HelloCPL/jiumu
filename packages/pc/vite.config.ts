74dc3b2b78cd606d86d5e6f6dbf79b8e0e0e6784
陈一支 <chenpeiliang@smartcitysz.com>
2023-04-11 09:37:18 +0800

    update: 修改配置

--- packages/pc/vite.config.ts
+++ packages/pc/vite.config.ts
@@ -85,24 +85,24 @@ export default defineConfig(({ mode }) => {
       //     }
       //   }
       // }
-      target: 'esnext',
-      rollupOptions: {
-        output: {
-          manualChunks: {
-            Vue: ['vue', 'vue-router'],
-            VuePdfEmbed: ['vue-pdf-embed'],
-            Vue3Pdf3: ['vue3-pdfjs'],
-            // DocxPreview: ['docx-preview'],
-            VMdEditor: ['@kangc/v-md-editor'],
-            WangEditor: ['@wangeditor/editor'],
-            HighLight: ['highlight.js'],
-            Jquery: ['jquery'],
-            Luckyexcel: ['luckyexcel'],
-            JiumuUtils: ['@jiumu/utils'],
-            GSAP: ['gsap']
-          }
-        }
-      }
+      target: 'esnext'
+      // rollupOptions: {
+      //   output: {
+      //     manualChunks: {
+      //       Vue: ['vue', 'vue-router'],
+      //       VuePdfEmbed: ['vue-pdf-embed'],
+      //       Vue3Pdf3: ['vue3-pdfjs'],
+      //       // DocxPreview: ['docx-preview'],
+      //       VMdEditor: ['@kangc/v-md-editor'],
+      //       WangEditor: ['@wangeditor/editor'],
+      //       HighLight: ['highlight.js'],
+      //       Jquery: ['jquery'],
+      //       Luckyexcel: ['luckyexcel'],
+      //       JiumuUtils: ['@jiumu/utils'],
+      //       GSAP: ['gsap']
+      //     }
+      //   }
+      // }
     }
   }
 })
