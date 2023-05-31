6408dc5bf1e40bb19cf6e19804336740a99ff11a
陈一支 <chenpeiliang@smartcitysz.com>
2023-05-31 10:56:31 +0800

    update: 测试

--- packages/pc/src/views/test/index.vue
+++ packages/pc/src/views/test/index.vue
@@ -1,6 +1,9 @@
 <template>
   <div class=" box">
     <BaseEcharts :option="option"></BaseEcharts>
+
+    <div class="wrapp bg-danger" id="box1" @click="handle1"> </div>
+    <div class="wrapp bg-primary" id="box2" @click="handle2"> </div>
   </div>
 </template>
 
@@ -18,6 +21,26 @@ setTimeout(() => {
 
 }, 1000)
 
+const myBox = ref(null)
+const handle1 = () => {
+  const box: any = document.getElementById('box1')
+  console.dir(box)
+  console.log(box.style.backgroundColor)
+  if(box.style.backgroundColor === 'green') {
+    box.style.backgroundColor = 'red'
+  } else {
+    box.style.backgroundColor = 'green'
+  }
+}
+
+let count = 1
+const  handle2 = () => {
+  count = count === 1 ? 2 : 1
+  const box: any = document.getElementById('box2')
+  const div = `<div style="position: absolute; top: 50px; left: 50px; width: 50px; height: 50px; background: ${count === 1 ? 'red' : 'green'}"><div>`
+  box.innerHTML = div
+}
+
 
 </script>
 
@@ -27,5 +50,12 @@ setTimeout(() => {
   width: 100%;
   min-height: 100vh;
   padding: 36px;
+}
+
+.wrapp {
+  margin-top: 20px;
+  width: 300px;
+  height: 300px;
+  position: relative;
 }
 </style>
